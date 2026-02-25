34596fedb29ec178723dd377c4e5456539f7200b
Lee DaSan <ddmt0805@gmail.com>
2026-02-26 01:15:14 +0900

    study: practice array example

---
+++ src/List/MainStringArrayArgument.java
@@ -0,0 +1,22 @@
+package List;
+
+public class MainStringArrayArgument {
+
+    public static void main(String[] args) {
+        if(args.length != 2) {
+            System.out.println("값의 수가 부족합니다.");
+            System.exit(0);
+        }
+
+        String strNum1 = args[0];
+        String strNum2 = args[1];
+        
+        int num1 = Integer.parseInt(strNum1); // 외부 인자에 숫자 넣으면 작동
+        int num2 = Integer.parseInt(strNum2);
+        
+        int result = num1 + num2;
+        System.out.println("result = " + result);
+
+
+    }
+}
