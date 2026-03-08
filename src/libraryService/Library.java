1a9d42dcf360050cd9fb589b693c9947f87c5a0a
Lee DaSan <ddmt0805@gmail.com>
2026-03-09 02:36:20 +0900

    study: library system update for OOP(object oriented programming)

--- src/libraryService/Library.java
+++ src/libraryService/Library.java
@@ -2,10 +2,13 @@ package libraryService;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Scanner;
 
 public class Library {
 
-    public List<Book>books = new ArrayList<>();
+    Scanner scanner = new Scanner(System.in);
+    private final List<Book>books = new ArrayList<>();
+
 
     public void registerBook(Book book) {
         books.add(book);
@@ -22,11 +25,47 @@ public class Library {
     public void printCanRental() {
         System.out.println("대여 가능한 책 목록:");
         for (Book book : books) {
-            if (book.getIsBook()) {
-                System.out.println(book.getTitle());
+            if (book.getHasBook()) {
+                System.out.printf("제목:%1$s, 저자:%2$s\n",book.getTitle(),book.getAuthor());
             }
         }
         System.out.println("----------------");
+    }
+    public void rentalBook(Member member) {
+        System.out.println("대여할 책의 이름과 저자를 입력해주세요.");
+        System.out.println("책 이름 :");
+        String title = scanner.nextLine();
+        System.out.println("책 저자 :");
+        String author = scanner.nextLine();
+        for (Book book : books) {
+            if (title.equals(book.getTitle()) && author.equals(book.getAuthor())) {
+                if (book.getHasBook()) {
+                    book.changeToCanNotRental();
+                    member.getRentBooks().add(book);
+                    System.out.println(book.getTitle() + "을(를) 대여하셨습니다.");
+                } else {
+                    System.out.println("대여 가능한 책이 아닙니다.");
+                }
+                return;
+            }
+        }
+        System.out.println("오류 : 대여할 책의 이름과 저자를 확인해주세요.");
+    }
+
+    public void returnBook(Member member) {
+        System.out.println("반납할 책의 이름과 저자를 입력해주세요.");
+        System.out.println("책 이름:");
+        String title = scanner.nextLine();
+        System.out.println("책 저자:");
+        String author = scanner.nextLine();
+        for (Book book :member.getRentBooks()) {
+            if (title.equals(book.getTitle()) && author.equals(book.getAuthor())) {
+                book.changeToCanRental();
+                member.getRentBooks().remove(book);
+                System.out.println(book.getTitle() + "을(를) 반납하셨습니다.");
+            }
+        }
+
     }
 
 
--- src/libraryService/Member.java
+++ src/libraryService/Member.java
@@ -36,46 +36,15 @@ public class Member {
         return id;
     }
 
-
-    // main method
+    public List<Book>getRentBooks() {
+        return rentBooks;
+    }
 
     public void printRentedBooks() {
+        System.out.println("현재 대여중인 책:");
         for (Book book : rentBooks) {
-            System.out.println(book.getTitle());
+            System.out.printf("제목:%1$s, 저자:%2$s\n",book.getTitle(),book.getAuthor());
         }
+        System.out.println("--------------------");
     }
-
-    public boolean isRentBook(Book book) {
-        boolean result = false;
-        for (Book other : rentBooks) {
-            if (other == book) {
-                result = true;
-                break;
-            }
-        }
-        return result;
-    }
-
-    public void rentalBook(Book book) {
-        out:
-        if (book.getIsBook()) {
-            book.changeToCanNotRental();
-            rentBooks.add(book);
-            System.out.println(book.getTitle() + "을(를) 대여하였습니다.");
-            break out;
-        } else {
-            System.out.println("대여가 불가능합니다.");
-        }
-    }
-
-    public void returnBook(Book book) {
-        if (isRentBook(book)) {
-            book.changeToCanRental();
-            rentBooks.remove(book);
-            System.out.println(book.getTitle() + "을(를) 반납하였습니다.");
-        } else {
-            System.out.println("대여한 책이 아닙니다.");
-        }
-    }
-
 }
