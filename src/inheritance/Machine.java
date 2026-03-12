424bcee701bd1c7d0bf801aee93412c57161c9fb
Lee DaSan <ddmt0805@gmail.com>
2026-03-12 22:59:35 +0900

    study:practice inheritance Example

---
+++ src/inheritance/Machine.java
@@ -0,0 +1,15 @@
+package inheritance;
+
+public class Machine {
+
+    void turnOn() {
+        System.out.println("시스템 시작");
+    }
+    void turnOff() {
+        System.out.println("시스템 종료");
+    }
+    void batteryChange() {
+        System.out.println("AA건전지 교체");
+    }
+}
+
